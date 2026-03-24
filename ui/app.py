cf4bf8b98ebb2183644236cef596755a4f4a7ec8
mohammed-genclays <mohammed.genclays@gmail.com>
2026-03-25 00:33:15 +0530

    docker template added

---
+++ ui/app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("Enterprise Data Platform")
+
+st.write("✅ Docker is working")
+st.write("✅ UI container is running")
+st.write("✅ You are building an enterprise system 🚀")
